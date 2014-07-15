e0dfd4f6d8bc1f976add25324e388e3db9ca15f1
pedro <pedro@otherway.es>
2014-07-15 09:09:58 +0200

    Initial commit

---
+++ l10n_es_aeat_mod340/__openerp__.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2011 Ting (http://www.ting.es) All Rights Reserved.
+#    Copyright (c) 2011-2013 Acysos S.L. (http://acysos.com) All Rights Reserved
+#                       Ignacio Ibeas Izquierdo <ignacio@acysos.com>
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name' : 'Generación de fichero modelo 340 y libro de IVA',
+    'version' : '2.0',
+    'author' : 'Acysos S.L., Francisco Pascual (Ting), Nan-tic',
+    'website' : 'www.acysos.com, www.ting.es, www.nan-tic.com',
+    'category' : 'Localisation/Accounting',
+    'description' : '''
+Módulo para la presentación del modelo 340. Exportación a formato AEAT. Libro de IVA
+
+Los impuestos incluidos en este modelo se indican en el Código base cuenta. Por defecto actualiza todos los código base que deban incluirse.
+Si el plan contable esta instalado recuerde utilizar account_chart_update para actualizar los códigos. Contabilidad y Finanzas -> Configuración -> Contabilidad Financiera -> Actualizar plan contable a partir de una plantila de plan contable
+
+Búsqueda de facturas emitidas y recibidas.
+Exportación a formato de AEAT de facturas emitidas y recibidas.
+Exportación de facturas con varios tipos impositivos. Clave de operación C.
+Facturas intracomunitarias excepto las operaciones a las que hace referencia el artículo 66 del RIVA que tienen un tratamiento especial.
+Facturas rectificativas.
+Facturas resumen de tiques.
+Permite imprimir el libro de IVA, basado en la misma legislación.
+
+--- COSAS PENDIENTES (TODO LIST) ----------------------------------------------
+
+Facturas bienes de inversión
+Facturas intracomunitarias. Operaciones a las que hace referencia el artículo 66 del RIVA.
+Asientos contables de resumen de tiques
+Exportación de asientos resumen de facturas
+''',
+    'license' : 'AGPL-3',
+    'depends' : ['account',
+                 'base_vat',
+                 'l10n_es',
+                 'l10n_es_aeat',
+                 'account_refund_original',
+                 'account_chart_update',
+                 ],
+    'data' : [
+        'report/report_view.xml',
+        'wizard/export_mod340_to_boe.xml',
+        'mod340_view.xml',
+        'mod340_workflow.xml',
+        'security/ir.model.access.csv',
+        'res_partner_view.xml',
+        'mod340_sequence.xml',
+        'account_invoice_view.xml',
+        'account_view.xml',
+        'taxes_data.xml',
+    ],
+    'installable' : True,
+    'active' : False,
+}
---
+++ l10n_es_toponyms/wizard/l10n_es_toponyms_wizard.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2009 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
+#                       Jordi Esteve <jesteve@zikzakmedia.com>
+#    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+#                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import os
+import openerp.tools
+from openerp.osv import fields, orm
+
+class config_es_toponyms(orm.TransientModel):
+    _name='config.es.toponyms'
+    _inherit = 'res.config.installer'
+
+    _columns = {
+        'name':fields.char('Name', size=64),
+        'state': fields.selection([('official', 'Official'),
+                                   ('spanish', 'Spanish'),
+                                   ('both', 'Both')], 'State names',
+                                  required=True),
+        'city_info': fields.selection([('yes', 'Yes'), ('no','No')],
+                                      'City information', required=True),
+    }
+
+    _defaults={
+        'state': 'official',
+        'city_info': 'yes',
+    }
+    
+    def create_states(self, cr, uid, state_type, context=None):
+        """Import spanish states information through an XML file."""
+        file_name = 'l10n_es_toponyms_states_%s.xml' %state_type
+        try:
+            fp = openerp.tools.file_open(os.path.join('l10n_es_toponyms',
+                                            os.path.join('wizard', file_name)))
+        except IOError, e:
+            fp = None
+        if fp:
+            idref = {}
+            openerp.tools.convert_xml_import(cr, 'l10n_es_toponyms', fp, idref,
+                                             'init', noupdate=True)
+            return True
+        return False
+
+    def create_zipcodes(self, cr, uid, context=None):
+        """Import spanish zipcodes information through an XML file."""
+        file_name = 'l10n_es_toponyms_zipcodes.xml'
+        try:
+            fp = openerp.tools.file_open(os.path.join('l10n_es_toponyms',
+                                            os.path.join('wizard', file_name)))
+        except IOError, e:
+            fp = None
+        if fp:
+            idref = {}
+            openerp.tools.convert_xml_import(cr, 'l10n_es_toponyms', fp, idref,
+                                             'init', noupdate=True)
+            return True
+        return False
+
+    def execute(self, cr, uid, ids, context=None):
+        if context is None: context = {}
+        super(config_es_toponyms, self).execute(cr, uid, ids, context=context)
+        res = self.read(cr, uid, ids)[0]
+        # Import spanish states (official, Spanish or both)
+        self.create_states(cr, uid, res['state'], context=context)
+        # Import spanish cities and zip codes
+        if res['city_info'] == 'yes':
+            self.create_zipcodes(cr, uid, context=context)
